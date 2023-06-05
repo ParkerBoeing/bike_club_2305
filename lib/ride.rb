989829d45d3dde5467adc928e3481cdfb65e4159
Parker Boeing <parker.boeing@gmail.com>
2023-06-05 09:15:44 -0600

    Feat: make a functioning ride class

---
+++ lib/ride.rb
@@ -0,0 +1,26 @@
+class Ride
+  attr_reader :name, :distance, :terrain, :loop
+  def initialize(ride)
+    @name = ride[:name]
+    @distance = ride[:distance]
+    @terrain = ride[:terrain]
+    @loop = ride[:loop]
+  end
+
+  def loop?
+    if loop
+      true
+    else
+      false
+    end
+  end
+
+  def total_distance
+    if loop == false
+      total_distance = @distance * 2
+    else
+      total_distance = @distance
+    end
+    total_distance
+  end
+end
