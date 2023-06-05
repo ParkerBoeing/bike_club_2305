8453c12955600079de6f159c1c6f9537a0e77537
Parker Boeing <parker.boeing@gmail.com>
2023-06-05 10:44:48 -0600

    Feat: assess which bikers can ride a certain trail

--- lib/bikeclub.rb
+++ lib/bikeclub.rb
@@ -10,7 +10,15 @@ class BikeClub
   end
 
   def can_ride?(ride)
-
+  can_ride = []
+  @bikers.each do |biker|
+      if biker.acceptable_terrain.include?(ride.terrain) && ride.total_distance < biker.max_distance
+        can_ride << biker
+      else
+        false
+      end
+    end
+    can_ride
   end
 
   def most_rides
@@ -18,6 +26,6 @@ class BikeClub
   end
 
   def best_time(ride)
-    
+
   end
 end
