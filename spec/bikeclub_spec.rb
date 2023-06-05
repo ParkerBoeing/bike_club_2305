0e16cef67e5d2ca4018ff577c27867440878410d
Parker Boeing <parker.boeing@gmail.com>
2023-06-05 10:45:09 -0600

    Test: can ride? method

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -21,4 +21,20 @@ describe BikeClub do
     ubc.add_biker(biker2)
     expect(ubc.bikers).to eq [biker, biker2]
   end
+
+  it 'can provide a list of eligible riders for a trail' do
+    ubc = BikeClub.new("Utah Bike Club")
+    biker = Biker.new("Kenny", 30)
+    biker.learn_terrain!(:gravel)
+    biker.learn_terrain!(:hills)
+    biker2 = Biker.new("Athena", 15)
+    biker2.learn_terrain!(:gravel)
+    biker2.learn_terrain!(:hills)
+    ubc.add_biker(biker)
+    ubc.add_biker(biker2)
+    ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    expect(ubc.can_ride?(ride1)).to eq [biker]
+    expect(ubc.can_ride?(ride2)).to eq [biker, biker2]
+  end
 end
